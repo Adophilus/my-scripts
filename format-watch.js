c0be5984cefc45d45f84514e286b15c891c55a9b
Adophilus <uchenna19of@gmail.com>
2022-07-25 10:48:02 +0100

    added support for external folders

--- format-watch.js
+++ format-watch.js
@@ -3,20 +3,31 @@ const fs = require('fs')
 const path = require('path')
 const { exec } = require('child_process')
 
-const extensions = ['.html', '.css', '.cjs', '.js', '.jsx', '.ts', '.tsx', '.md', '.json']
+const workdir = process.cwd() || process.argv[1]
+const extensions = [
+  '.html',
+  '.css',
+  '.cjs',
+  '.js',
+  '.jsx',
+  '.ts',
+  '.tsx',
+  '.md',
+  '.json'
+]
 const prettify = (event, filePath) => {
   if (event === 'update') {
     for (let extension of extensions) {
       if (filePath.endsWith(extension) && event === 'update') {
         console.log(`prettifying ${filePath}`)
-        exec(`pnpm format:prettier --write "${path.join(process.cwd(), filePath)}"`)
+        exec(`pnpm format:prettier --write "${filePath}"`)
       }
     }
   }
 }
 
 watch(
-  './',
+  workdir,
   {
     recursive: true,
     filter: (f) => !/node_modules|\.git|\.next|\.public/.test(f)
